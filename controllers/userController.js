9ec58af0809a9293581d41127eae58cfac768614
devian5 <acgarage@outlook.es>
2021-02-23 10:47:14 +0100

    add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require('../models/car');
+
+class UserController {
+
+    constructor() {
+    }
+
+    async indexAll() {
+        return User.find().limit(10);
+    }
+
+    async store(user) {
+        return User.create(user);
+    }
+
+    async update(id, user) {    
+        return User.findByIdAndUpdate(id,user);
+    }
+
+    async destroy(id) {
+        return User.findByIdAndRemove(id);
+    }
+
+}
+
+
+let userController = new UserController();
+module.exports = userController;
