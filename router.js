be82a39c0f8e84e11c81f25ddde8e2c12a5c85d7
devian5 <acgarage@outlook.es>
2021-02-23 10:57:28 +0100

    add user router

--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const carRouter = require('./routers/carRouter');
+const userRouter = require('./routers/userRouter');
 
 
 router.use('/cars',carRouter);
+router.use('/user',userRouter);
 
 
 module.exports = router;
---
+++ routers/userRouter.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const carsController = require('../controllers/userController');
+
+router.get('/',async (req, res) => {   
+    try{
+        res.json(await carsController.indexAll())
+    } catch (error) {
+        return res.sendStatus(500).json({
+            message: 'Server Error'
+        });
+    }
+});
+
+router.post('/',async (req, res) => {
+    try{
+        const id = await carsController.store(req.body);
+        const status = 'success';
+        res.json({status,id});
+    } catch( error ){
+        return res.sendStatus(500).json({
+            message: 'Server Error'
+        });
+    }
+})
+
+router.put('/:id',async (req,res) => {
+    try{
+        const id = req.params.id;
+        res.json(await carsController.update(id,req.body));
+    } catch( error ){
+        return res.sendStatus(500).json({
+            message: 'Server Error'
+        });
+    }
+});
+
+router.delete('/:id',async (req, res) => {
+   try{
+        const id = req.params.id;
+        const status = 'deleted'
+        await carsController.destroy(id);
+        res.json({status,id});
+   } catch( error ) {
+    return res.sendStatus(500).json({
+        message: 'Server Error'
+    });   
+   }
+
+});
+
+
+
+module.exports = router;
